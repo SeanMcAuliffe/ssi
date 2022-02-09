e62a01af2c1f887c53a0e236a27c85a29bfb22f3
SeanMcAuliffe <sean.tristan.mcauliffe@gmail.com>
2022-02-08 21:20:58 -0800

    Step 1, prompt

--- simple_interpreter/ssi.c
+++ simple_interpreter/ssi.c
@@ -5,18 +5,79 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <poll.h>
 
-#define MAX_SIZE 80
+
+#define MAX_USERNAME_SIZE 255
+#define MAX_HOSTNAME_SIZE 255
+#define MAX_INPUT_SIZE 255
+#define MAX_PATH_LENGTH 1024
+
+
+void clear_buffer(char* buffer) {
+    for (int i = 0; i < MAX_INPUT_SIZE; i++) {
+        buffer[i] = 0;
+    }
+}
+
+bool is_ongoing(const char* buffer, ssize_t len) {
+
+    char* literal = "exit\0";
+    bool result = true;
+
+    if (strcmp(buffer, "\n") == 0){
+        return true;
+    }
+
+    for (int i = 0; i < len - 1; i++) {
+        if (buffer[i] != literal[i]) {
+            result = false;
+        }
+    }
+
+    return !result;
+}
+
 
 int main() {
+
+    /* Input buffers */
     char* user_login = NULL;
-    char host_name[MAX_SIZE];
+    char* user_input_buffer = NULL;
+    char host_name[MAX_HOSTNAME_SIZE] = {0};
+    char current_directory[MAX_PATH_LENGTH] = {0};
+    bool ongoing = true;
+
+
+    /* return code */
+    int rc = -1;
+    ssize_t num_read = 0;
+    size_t len = 0;
+
+    /* Get info for prompt */
+    rc = gethostname(host_name, sizeof(host_name));
+    if (rc != 0) {
+        printf("Error in gethostname()\n");
+    }
+
     user_login = getlogin();
-    if (user_login == NULL){
-        printf("No login detected. Setting default.\n");
+    if (user_login == NULL) {
         user_login = "default";
     }
-    gethostname(host_name, MAX_SIZE*sizeof(char));
-    printf("%s@%s >> \n", user_login, host_name);
+
+    getcwd(current_directory, sizeof(current_directory));
+
+    printf("%s@%s: %s > ", user_login, host_name, current_directory);
+
+    while (ongoing) {
+        num_read = getline(&user_input_buffer, &len, stdin);
+        ongoing = is_ongoing(user_input_buffer, num_read);
+        clear_buffer(user_input_buffer);
+        getcwd(current_directory, sizeof(current_directory));
+        printf("%s@%s: %s > ", user_login, host_name, current_directory);
+    }
+
+    printf("\n");
     return 0;
 }
